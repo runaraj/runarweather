6707e5e12b33a2ac5530421a66ed7d4edd966a27
Runar Ask Johannessen <RUAJ@statoil.com>
2018-06-25 13:57:53 +0200

    get one temperature from yr

--- runarweather/app.js
+++ runarweather/app.js
@@ -6,10 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var yrRouter = require('./routes/yr');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- runarweather/client/src/App.js
+++ runarweather/client/src/App.js
@@ -3,26 +3,36 @@ import './App.css';
 
 class App extends Component {
   state = {users: []}
-  url = "http://www.yr.no/place/Norway/Akershus/B%C3%A6rum/Skui/forecast_hour_by_hour.xml"
+  url = "https://cors-anywhere.herokuapp.com/https://www.yr.no/place/Norway/Akershus/B%C3%A6rum/Sandvika/forecast_hour_by_hour.xml"
+  parseString = require('react-native-xml2js').parseString;
 
   componentDidMount() {
-    // fetch('/users')
-    //   .then(res => res.json())
-    //   .then(users => this.setState({ users }));
-    
+/*     fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users })); */
 
-    
+
+      fetch(this.url)
+        .then(response => response.text())
+        .then((response) => {
+          let out;
+          this.parseString(response, function(err, result) {
+            out = result.weatherdata.forecast[0].tabular[0].time[0].temperature[0].$.value
+          })
+          this.setState({temperature: out});
+        })
+        .catch((err) => {
+            console.log('fetch', err)
+        })
+
+      
   }
-
 
   render() {
     return (
       <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(data =>
-          <div key={data}>{data}</div>
-        )}
-        <h2>{this.state.users}</h2>
+        <h1>Temperature</h1>
+        <div>{this.state.temperature}</div>
       </div>
     );
   }
