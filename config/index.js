b0969b8eccffa16845d54eb679f339e6ce72b578
Runar Ask Johannessen <RUAJ@statoil.com>
2018-06-25 09:28:00 +0200

    installed express

--- config/index.js
+++ config/index.js
@@ -1,13 +1,12 @@
-var http = require('http');
 
-var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello NOT World!");
+const express = require('express');
+const app = express();
 
-});
+app.get('/', function (req, res) {
+  res.send('Hello Express World!')
+})
 
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
