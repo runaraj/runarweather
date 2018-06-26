71661849dc2fbbc5fdb78fe5c8824eac3dcd440b
--global <runar.johannessen96@gmail.com>
2018-06-26 09:18:50 +0200

    revert to default index

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/../public/build/index.html'));
-  // res.render('index', { title: 'Express' });
-  
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
