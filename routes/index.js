87b0cff3bec9957f2e96d2b5769dd5ffbd459a16
--global <runar.johannessen96@gmail.com>
2018-06-26 08:55:36 +0200

    set react index as home

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname + '/public/build/index.html'));
+  // res.render('index', { title: 'Express' });
+  
 });
 
 module.exports = router;
