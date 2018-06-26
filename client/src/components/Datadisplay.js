b18f1b39f2fcc508d963bd33507a54c7de42bf8c
Runar Ask Johannessen <runaraj@users.noreply.github.com>
2018-06-26 13:30:03 +0200

    Test deployment (#1)
    * test CI/CD
    
    * added config folder
    
    * installed express
    
    * restarted as react project
    
    * react project init
    
    * get one temperature from yr
    
    * added web.config file
    
    * new web config file
    
    * deleted web config file
    
    * decouple express and react
    
    * remove client for test
    
    * add react build folder
    
    * moved everything one level up
    
    * added client back
    
    * set react index as home
    
    * added path variable to file
    
    * pathname for index
    
    * pathname for home
    
    * add web.config file
    
    * revert to default index
    
    * build on site
    
    * fixed index route
    
    * removed view engine
    
    * simple express start script
    
    * tets with pushed build
    
    * added base components
    
    * updated react build
    
    * created api.js file

---
+++ client/src/components/Datadisplay.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import './Selector.css';
+
+class Datadisplay extends Component {
+    
+
+
+
+
+    render() {
+        return (
+            <div>I am the one who displays</div>
+        );
+    }
+}
+
+export default Datadisplay;
