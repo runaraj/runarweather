c1abff43cbeb67cd1516ff78519c473d4b68a982
--global <runar.johannessen96@gmail.com>
2018-06-26 09:08:42 +0200

    pathname for home

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '../public/build/index.html'));
+  res.sendFile(path.join(__dirname + '/../public/build/index.html'));
   // res.render('index', { title: 'Express' });
   
 });
