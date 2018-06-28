ccb5ed287132acfde7096713df39a7d3c597b4c9
--global <runar.johannessen96@gmail.com>
2018-06-28 09:02:51 +0200

    testing only backend

--- routes/users.js
+++ routes/users.js
@@ -12,10 +12,11 @@ router.get('/', function(req, res, next) {
   res.json([{
   	id: 1,
   	username: "samsepi0l"
-  }, {
-  	id: 2,
-  	username: "D0loresH4ze"
-  }]);
+  }, 
+  // {
+  // 	id: 2,
+  // 	username: "D0loresH4ze"}
+]);
 });
 
 module.exports = router;
