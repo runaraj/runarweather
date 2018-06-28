a9c44d531511d995817f9388bdc569dabbe81e40
--global <runar.johannessen96@gmail.com>
2018-06-28 09:25:20 +0200

    allow CORS

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ var apiRouter = require('./routes/api');
 var app = express();
 // var apiRouter = express.Router();
 
-/* app.use(function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-}); */
+});
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
